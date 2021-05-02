9e088c7f0b0e465ce767a1b4070928dacc7667a9
ArcangelSKY <angelzamoranooses@hotmail.com>
2021-05-01 21:03:00 -0400

    ultima version de la prueba

--- app/src/main/java/com/example/modulo4pruebaindividualfinal/MainActivity.kt
+++ app/src/main/java/com/example/modulo4pruebaindividualfinal/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.modulo4pruebaindividualfinal
 
+
+
+
+
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
-import android.widget.SearchView
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import layout.PerroAdaptador
-import org.jetbrains.anko.alert
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import retrofit2.Retrofit
@@ -22,31 +24,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // asigna la variable con la vista
         RecyclerView = findViewById(R.id.recyclerview)
-        searchByName()
+
+        // se llama el metodo para la ejecucion del programa
+        consumoApi()
     }
 
-    private fun initCharacter(puppies:PerroRespuesta) {
-
+    private fun adapatdorMasRecicler(puppies:PerroRespuesta) {
+        // metodo para llevar los datos al adapter y que se puedan mostrar las imagenes en listas
         perroAdaptador = PerroAdaptador(puppies.imagenes)
         RecyclerView.setHasFixedSize(true)
         RecyclerView.layoutManager = LinearLayoutManager(this)
 
         RecyclerView.adapter = perroAdaptador
     }
-
-    private fun searchByName() {
+    // metodo consume la api de la pagina web
+    private fun consumoApi() {
+        // se sincroniza o realiza al busqueda de los elementos dentro de la api
         doAsync {
             val call = getRetrofit().create(ServicioAPI::class.java).tomarPorNombre("mix/images").execute()
             val puppies = call.body() as PerroRespuesta
             uiThread {
 
-                    initCharacter(puppies)
+                    adapatdorMasRecicler(puppies)
 
             }
         }
     }
-
+    // se tiene la datos de la página web dentro de la api
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder().baseUrl("https://dog.ceo/api/breed/").addConverterFactory(
             GsonConverterFactory.create()).build()
--- app/src/main/java/com/example/modulo4pruebaindividualfinal/PerroRespuesta.kt
+++ app/src/main/java/com/example/modulo4pruebaindividualfinal/PerroRespuesta.kt
@@ -1,7 +1,7 @@
 package com.example.modulo4pruebaindividualfinal
 
 import com.google.gson.annotations.SerializedName
-
+// la data de los datos dentro de la api
 data class PerroRespuesta( // se crea la data para las listas donde esta la api
     @SerializedName("status") var estatus:String,
     @SerializedName("message") var imagenes:List<String>
